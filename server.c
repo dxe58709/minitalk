c0b2d01ddd61ec29e53225da7d75fbbc547f7d49
Nao Sakanoue <nsakanou@c1r16s6.42tokyo.jp>
2023-08-20 17:49:35 +0900

    makefile

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 14:46:30 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/08/20 16:44:16 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/08/20 17:37:57 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	signal_handler(int signum)
 {
-	static int				i = 0;
+	static int				bit_position = 0;
 	static unsigned char	c = 0;
 
 	if (signum == SIGUSR1)
-		c |= (1 << i);
-	i++;
-	if (i == 8)
+		c |= (1 << bit_position);
+	bit_position++;
+	if (bit_position == 8)
 	{
 		write(STDOUT_FILENO, &c, 1);
-		i = 0;
+		bit_position = 0;
 		c = 0;
 	}
 }
