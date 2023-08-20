415ada80df97a6e5b4e36d5b69968249e2eed1d7
Nao Sakanoue <nsakanou@c1r16s6.42tokyo.jp>
2023-08-20 16:07:19 +0900

    printf

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 14:46:30 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/08/17 15:02:08 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/08/20 16:07:00 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 void	signal_handler(int signum)
 {
-	static int				i;
-	static unsigned char	c;
+	static int				i = 0;
+	static unsigned char	c = 0;
 
-	i = 0;
-	c = 0;
 	if (signum == SIGUSR1)
 		c |= (1 << i);
 	i++;
@@ -28,12 +26,11 @@ void	signal_handler(int signum)
 		i = 0;
 		c = 0;
 	}
-	kill(client, SIGUSR2);//c?
 }
 
 int	main(void)
 {
-	printf("Server PID: %d\n", getpid());
+	printf("Server PID: %d\n", getpid());//自作printfにする
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	while (1)
